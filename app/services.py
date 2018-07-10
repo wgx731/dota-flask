b6778191f6cb4fea136d2a91af4d52d70aa16be8
wgx731 <wgx731@gmail.com>
2018-07-10 11:38:42 +0800

    [#1]: refactor view and service

--- app/services.py
+++ app/services.py
@@ -2,6 +2,25 @@ from datetime import date
 from app.models import Score
 
 
+def accept_json(request):
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
+
+
+def sort_score_list(score_list, sort_by):
+    if sort_by == 'W':
+        return sorted(score_list, key=lambda s: s.week_score, reverse=True)
+    elif sort_by == 'M':
+        return sorted(score_list, key=lambda s: s.month_score, reverse=True)
+    elif sort_by == 'Y':
+        return sorted(score_list, key=lambda s: s.year_score, reverse=True)
+    else:
+        return sorted(score_list, key=lambda s: s.overall_score, reverse=True)
+
+
 def get_player_scores(player_id_list):
     player_list = Score.query\
             .filter(Score.score_date == date.today())\
--- app/views.py
+++ app/views.py
@@ -1,24 +1,7 @@
 from app import app
-from app.services import get_player_scores
+from app.services import get_player_scores, accept_json, sort_score_list
 from flask import request, abort, render_template, jsonify
 
-
-def accept_json():
-    best = request.accept_mimetypes \
-        .best_match(['application/json', 'text/html'])
-    return best == 'application/json' and \
-        request.accept_mimetypes[best] > \
-        request.accept_mimetypes['text/html']
-
-def sort_score_list(score_list, sort_by):
-    if sort_by == 'W':
-        return sorted(score_list, key=lambda s: s.week_score, reverse=True)
-    elif sort_by == 'M':
-        return sorted(score_list, key=lambda s: s.month_score, reverse=True)
-    elif sort_by == 'Y':
-        return sorted(score_list, key=lambda s: s.year_score, reverse=True)
-    else:
-        return sorted(score_list, key=lambda s: s.overall_score, reverse=True)
 
 @app.route('/leaderboard', methods=['GET'])
 def leader_board():
@@ -38,10 +21,10 @@ def leader_board():
         sort_by = 'O'
         app.logger.warning(e)
     score_list = sort_score_list(score_list, sort_by)
-    if accept_json():
+    if accept_json(request):
         return jsonify([s.to_dict() for s in score_list])
     else:
         return render_template(
-            'leaderboard.html',
+            'leader_board.html',
             scores=score_list
         )
