df2415af19fbbe253303e0e5fc5e26c92eeb736b
wgx731 <wgx731@gmail.com>
2018-07-10 11:28:29 +0800

    [#1]: add sort list by GET param

--- app/views.py
+++ app/views.py
@@ -10,6 +10,15 @@ def accept_json():
         request.accept_mimetypes[best] > \
         request.accept_mimetypes['text/html']
 
+def sort_score_list(score_list, sort_by):
+    if sort_by == 'W':
+        return sorted(score_list, key=lambda s: s.week_score, reverse=True)
+    elif sort_by == 'M':
+        return sorted(score_list, key=lambda s: s.month_score, reverse=True)
+    elif sort_by == 'Y':
+        return sorted(score_list, key=lambda s: s.year_score, reverse=True)
+    else:
+        return sorted(score_list, key=lambda s: s.overall_score, reverse=True)
 
 @app.route('/leaderboard', methods=['GET'])
 def leader_board():
@@ -23,7 +32,12 @@ def leader_board():
     score_list = get_player_scores(player_id_list)
     if len(score_list) == 0:
         return abort(404)
-    # TODO: add sort here
+    try:
+        sort_by = request.args.get("sort").upper()
+    except AttributeError as e:
+        sort_by = 'O'
+        app.logger.warning(e)
+    score_list = sort_score_list(score_list, sort_by)
     if accept_json():
         return jsonify([s.to_dict() for s in score_list])
     else:
